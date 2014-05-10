d8e4dac7bff596dc4c94b03bf603255a00564e7e
Johannes Hoppe <info@johanneshoppe.com>
2014-05-10 15:08:55 +0200

    switches to MIT License

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
