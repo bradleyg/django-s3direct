5f076824c10d151d6fb70884cec900cfa250a72f
bradley griffiths <bradley.griffiths@gmail.com>
2014-07-23 07:42:00 +0200

    Don't use a separate upload to param.

--- s3direct/urls.py
+++ s3direct/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, url
 from s3direct.views import get_upload_params
 
 urlpatterns = patterns('',
-    url('^get_upload_params/(?P<upload_to>.*)',
-            get_upload_params, name='s3direct'),
+    url('^get_upload_params/', get_upload_params, name='s3direct'),
 )
