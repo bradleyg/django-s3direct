72ca12afc952e9668a6c44edd19f1b5346ce83a2
bradley griffiths <bradley.griffiths@gmail.com>
2014-07-23 07:42:51 +0200

    Simplify widget.

--- s3direct/widgets.py
+++ s3direct/widgets.py
@@ -1,24 +1,24 @@
 import os
+
 from django.forms import widgets
 from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
-from django.conf import settings
 
 
-HTML = (
-    '<div class="s3direct" data-policy-url="{policy_url}">'
-    '    <a class="file-link" target="_blank" href="{file_url}">{file_name}</a>'
-    '    <a class="file-remove" href="#remove">Remove</a>'
-    '    <input class="file-url" type="hidden" value="{file_url}" id="{element_id}" name="{name}" />'
-    '    <input class="file-input" type="file" />'
-    '    <div class="progress progress-striped active">'
-    '        <div class="bar"></div>'
-    '    </div>'
-    '</div>'
-)
+class S3DirectWidget(widgets.TextInput):
 
-
-class S3DirectEditor(widgets.TextInput):
+    html = (
+        '<div class="s3direct" data-policy-url="{policy_url}">'
+        '  <a class="file-link" target="_blank" href="{file_url}">{file_name}</a>'
+        '  <a class="file-remove" href="#remove">Remove</a>'
+        '  <input class="file-url" type="hidden" value="{file_url}" id="{element_id}" name="{name}" />'
+        '  <input class="file-upload-to" type="hidden" value="{upload_to}">'
+        '  <input class="file-input" type="file" />'
+        '  <div class="progress progress-striped active">'
+        '    <div class="bar"></div>'
+        '  </div>'
+        '</div>'
+    )
 
     class Media:
         js = (
@@ -31,23 +31,18 @@ class S3DirectEditor(widgets.TextInput):
             )
         }
 
-    def __init__(self, *args, **kwargs):
-        self.upload_to = kwargs.pop('upload_to', '')
-        super(S3DirectEditor, self).__init__(*args, **kwargs)
+    def __init__(self, upload_to):
+        self.upload_to = upload_to
+        super(S3DirectWidget, self).__init__()
 
-    def render(self, name, value, attrs=None):
-        final_attrs = self.build_attrs(attrs)
-        element_id = final_attrs.get('id')
-        kwargs = {'upload_to': self.upload_to}
+    def render(self, name, value='', attrs=None):
 
-        policy_url = reverse('s3direct', kwargs=kwargs)
-        file_url = value if value else ''
-        file_name = os.path.basename(file_url)
-
-        output = HTML.format(policy_url=policy_url,
-                             file_url=file_url,
-                             file_name=file_name,
-                             element_id=element_id,
-                             name=name)
+        output = self.html.format(
+                policy_url=reverse('s3direct'),
+                element_id=self.build_attrs(attrs).get('id'),
+                file_name=os.path.basename(value),
+                file_url=value,
+                name=name,
+                upload_to=self.upload_to)
 
         return mark_safe(output)
