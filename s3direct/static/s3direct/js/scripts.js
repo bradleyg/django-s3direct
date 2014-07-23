9616567d8601094bea5435bbcacb26c86f98e991
bradley griffiths <bradley.griffiths@gmail.com>
2014-07-23 14:22:38 +0200

    Use strict.

--- s3direct/static/s3direct/js/scripts.js
+++ s3direct/static/s3direct/js/scripts.js
@@ -1,8 +1,10 @@
 (function(){
 
+    "use strict"
+
     var getCookie = function(name) {
-        var value = '; ' + document.cookie
-        var parts = value.split('; ' + name + '=')
+        var value = '; ' + document.cookie,
+            parts = value.split('; ' + name + '=')
         if (parts.length == 2) return parts.pop().split(';').shift()
     }
 
