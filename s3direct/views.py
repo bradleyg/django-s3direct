c854c5d1f089c5064a9271002cee2a854181810b
bradley griffiths <bradley.griffiths@gmail.com>
2014-07-27 22:37:23 +0200

    Don't use auth decorator.

--- s3direct/views.py
+++ s3direct/views.py
@@ -1,8 +1,8 @@
 import json
 
 from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
 from django.views.decorators.http import require_POST
+from django.core.exceptions import PermissionDenied
 from django.conf import settings
 
 from .utils import create_upload_data
@@ -12,8 +12,10 @@ S3DIRECT_AUTH_TEST = getattr(settings, 'S3DIRECT_AUTH_TEST', lambda u: True)
 
 
 @require_POST
-@user_passes_test(S3DIRECT_AUTH_TEST)
 def get_upload_params(request):
+    if not S3DIRECT_AUTH_TEST(request.user):
+        raise PermissionDenied()
+
     content_type = request.POST['type']
     source_filename = request.POST['name']
     upload_to = request.POST['upload_to']
