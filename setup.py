85de9cc111067fd6ad0365e04a664f278c37aed7
Bradley Griffiths <bradley.griffiths@gmail.com>
2014-10-22 00:17:07 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='django-s3direct',
-    version='0.3.4',
+    version='0.3.5',
     description='Add direct uploads to S3 functionality with a progress bar to file input fields.',
     long_description=readme,
     author="Bradley Griffiths",
