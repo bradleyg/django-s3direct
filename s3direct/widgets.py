c684356530dc5c8d7d550fb3dba2d9e04cf7f180
Ezequiel Golub <ezequiel.golub@gmail.com>
2016-08-08 02:21:33 -0300

    adding style

--- s3direct/widgets.py
+++ s3direct/widgets.py
@@ -10,12 +10,12 @@ from django.utils.http import urlunquote_plus
 class S3DirectWidget(widgets.TextInput):
 
     default_html = (
-        '<div class="s3direct" data-policy-url="{policy_url}" style="{style}">'
+        '<div class="s3direct" data-policy-url="{policy_url}">'
         '  <a class="file-link" target="_blank" href="{file_url}">{file_name}</a>'
         '  <a class="file-remove" href="#remove">Remove</a>'
         '  <input class="file-url" type="hidden" value="{file_url}" id="{element_id}" name="{name}" />'
         '  <input class="file-dest" type="hidden" value="{dest}">'
-        '  <input class="file-input" type="file" />'
+        '  <input class="file-input" type="file"  style="{style}"/>'
         '  <div class="progress progress-striped active">'
         '    <div class="bar"></div>'
         '  </div>'
