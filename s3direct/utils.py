e583400de98a4fe9d16a10a51dd5e3dda13c6115
Ben Jeffrey <mail@benjeffrey.net>
2017-06-02 19:22:47 +0100

    Remove support for old positional settings.

--- s3direct/utils.py
+++ s3direct/utils.py
@@ -12,36 +12,12 @@ def get_at(index, t):
     return value
 
 
-# NOTE: Don't use constant as it will break ability to change at runtime
-# (E.g. tests)
 def get_s3direct_destinations():
-    """Returns s3direct destinations, converting old format if necessary."""
-    destinations = getattr(settings, 'S3DIRECT_DESTINATIONS', None)
-    if destinations is None:
-        return None
+    """Returns s3direct destinations.
 
-    # TODO: Remove when older "positional" settings are no longer supported
-    converted_destinations = {}
-    key_mapping = {
-        0: 'key',
-        1: 'auth',
-        2: 'allowed',
-        3: 'acl',
-        4: 'bucket',
-        5: 'cache_control',
-        6: 'content_disposition',
-        7: 'server_side_encryption',
-    }
-    if destinations:
-        for dest, dest_value in destinations.items():
-            if type(dest_value) is tuple or type(dest_value) is list:
-                converted_destinations[dest] = {}
-                for index, key_name in key_mapping.items():
-                    converted_destinations[dest][key_name] = get_at(
-                            index, dest_value)
-            else:
-                converted_destinations[dest] = dest_value
-    return converted_destinations
+    NOTE: Don't use constant as it will break ability to change at runtime (e.g. tests)
+    """
+    return getattr(settings, 'S3DIRECT_DESTINATIONS', None)
 
 
 # AWS Signature v4 Key derivation functions. See:
