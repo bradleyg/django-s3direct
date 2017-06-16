94cc1c696d3b8a9a9ef68d6087d18f4bf5718268
Al Scott <al@yunojuno.com>
2017-06-16 16:01:11 +0100

    fix error event dispatch, also refactor some of the variable naming

--- example/static/s3direct/js/django-s3-uploads.js
+++ example/static/s3direct/js/django-s3-uploads.js
@@ -169,36 +169,40 @@ var View = function View(element, store) {
                 url = (0, _store.getUrl)(store),
                 error = (0, _store.getError)(store);
 
+            // if there is a filename, we want to display it and a "remove" link
             if (filename) {
-                var link = element.querySelector('.file-link');
+                this.$link.innerHTML = filename;
+                this.$link.setAttribute('href', url);
 
-                link.innerHTML = filename;
-                link.setAttribute('href', url);
-
-                element.classList.add('link-active');
-                element.classList.remove('form-active');
-            } else {
-                element.querySelector('.file-url').value = '';
-                element.querySelector('.file-input').value = '';
-
-                element.classList.add('form-active');
-                element.classList.remove('link-active');
+                this.$element.classList.add('link-active');
+                this.$element.classList.remove('form-active');
             }
+            // if not, let's empty the form and revert to default state
+            else {
+                    this.$element.querySelector('.file-url').value = '';
+                    this.$element.querySelector('.file-input').value = '';
 
+                    this.$element.classList.add('form-active');
+                    this.$element.classList.remove('link-active');
+                }
+
+            // if there's an error, let's display it
             if (error) {
-                element.classList.add('has-error');
-                element.classList.add('form-active');
-                element.classList.remove('link-active');
+                this.$element.classList.add('has-error');
+                this.$element.classList.add('form-active');
+                this.$element.classList.remove('link-active');
 
-                element.querySelector('.file-input').value = '';
-                element.querySelector('.error').innerHTML = error;
+                this.$element.querySelector('.file-input').value = '';
+                this.$element.querySelector('.error').innerHTML = error;
 
                 // dispatch event on the element for external use
-                (0, _utils.raiseEvent)(element, 's3uploads:error', error);
-            } else {
-                element.classList.remove('has-error');
-                element.querySelector('.error').innerHTML = '';
+                (0, _utils.raiseEvent)(this.$element, 's3uploads:error', { error: error });
             }
+            // if not, lets empty and hide the error div
+            else {
+                    this.$element.classList.remove('has-error');
+                    this.$element.querySelector('.error').innerHTML = '';
+                }
         },
 
         removeUpload: function removeUpload(event) {
@@ -206,29 +210,34 @@ var View = function View(element, store) {
         },
 
         getUploadURL: function getUploadURL(event) {
-            var file = element.querySelector('.file-input').files[0],
-                dest = element.querySelector('.file-dest').value,
-                url = element.getAttribute('data-policy-url');
+            var file = this.$input.files[0],
+                dest = this.$dest.value,
+                url = this.$element.getAttribute('data-policy-url');
 
             store.dispatch((0, _actions.clearErrors)());
             store.dispatch((0, _actions.getUploadURL)(file, dest, url, store));
         },
 
         init: function init() {
-            console.log(element, store);
+            // cache all the query selectors
+            this.$element = element;
+            this.$url = element.querySelector('.file-url');
+            this.$input = element.querySelector('.file-input');
+            this.$remove = element.querySelector('.file-remove');
+            this.$dest = element.querySelector('.file-dest');
+            this.$link = element.querySelector('.file-link');
+            this.$error = element.querySelector('.error');
 
-            var url = element.querySelector('.file-url'),
-                input = element.querySelector('.file-input'),
-                remove = element.querySelector('.file-remove'),
-                status = url.value === '' ? 'form' : 'link';
+            // set initial DOM state
+            var status = this.$url.value === '' ? 'form' : 'link';
+            this.$element.className = 's3direct ' + status + '-active';
 
-            element.className = 's3direct ' + status + '-active';
+            // add event listeners
+            this.$remove.addEventListener('click', this.removeUpload.bind(this));
+            this.$input.addEventListener('change', this.getUploadURL.bind(this)
 
-            // store.subscribe(this.updateScore.bind(this))
-            remove.addEventListener('click', this.removeUpload.bind(this));
-            input.addEventListener('change', this.getUploadURL.bind(this));
-
-            store.subscribe(this.render.bind(this));
+            // subscribe to the store so that we can reactively render changes
+            );store.subscribe(this.render.bind(this));
         }
     };
 };
--- s3direct/src/app/components/index.js
+++ s3direct/src/app/components/index.js
@@ -11,37 +11,39 @@ const View = function(element, store) {
                 url = getUrl(store),
                 error = getError(store);
 
+            // if there is a filename, we want to display it and a "remove" link
             if (filename) {
-                const link = element.querySelector('.file-link');
+                this.$link.innerHTML = filename;
+                this.$link.setAttribute('href', url);
 
-                link.innerHTML = filename;
-                link.setAttribute('href', url);
-
-                element.classList.add('link-active');
-                element.classList.remove('form-active');
+                this.$element.classList.add('link-active');
+                this.$element.classList.remove('form-active');
             }
+            // if not, let's empty the form and revert to default state
             else {
-                element.querySelector('.file-url').value = '';
-                element.querySelector('.file-input').value = '';
+                this.$element.querySelector('.file-url').value = '';
+                this.$element.querySelector('.file-input').value = '';
 
-                element.classList.add('form-active');
-                element.classList.remove('link-active');
+                this.$element.classList.add('form-active');
+                this.$element.classList.remove('link-active');
             }
 
+            // if there's an error, let's display it
             if (error) {
-                element.classList.add('has-error');
-                element.classList.add('form-active');
-                element.classList.remove('link-active');
+                this.$element.classList.add('has-error');
+                this.$element.classList.add('form-active');
+                this.$element.classList.remove('link-active');
 
-                element.querySelector('.file-input').value = '';
-                element.querySelector('.error').innerHTML = error;
+                this.$element.querySelector('.file-input').value = '';
+                this.$element.querySelector('.error').innerHTML = error;
 
                 // dispatch event on the element for external use
-                raiseEvent(element, 's3uploads:error', error);
+                raiseEvent(this.$element, 's3uploads:error', {error});
             }
+            // if not, lets empty and hide the error div
             else {
-                element.classList.remove('has-error');
-                element.querySelector('.error').innerHTML = '';
+                this.$element.classList.remove('has-error');
+                this.$element.querySelector('.error').innerHTML = '';
             }
         },
 
@@ -50,29 +52,33 @@ const View = function(element, store) {
         },
 
         getUploadURL: function(event) {
-            const file = element.querySelector('.file-input').files[0],
-                dest = element.querySelector('.file-dest').value,
-                url  = element.getAttribute('data-policy-url');
+            const file = this.$input.files[0],
+                dest = this.$dest.value,
+                url  = this.$element.getAttribute('data-policy-url');
 
             store.dispatch(clearErrors());
             store.dispatch(getUploadURL(file, dest, url, store));
         },
 
         init: function() {
-            console.log(element, store);
+            // cache all the query selectors
+            this.$element = element;
+            this.$url     = element.querySelector('.file-url');
+            this.$input   = element.querySelector('.file-input');
+            this.$remove  = element.querySelector('.file-remove');
+            this.$dest    = element.querySelector('.file-dest');
+            this.$link    = element.querySelector('.file-link');
+            this.$error   = element.querySelector('.error');
 
-            var url = element.querySelector('.file-url'),
-            input  = element.querySelector('.file-input'),
-            remove = element.querySelector('.file-remove'),
-            status = (url.value === '') ? 'form' : 'link'
+            // set initial DOM state
+            const status = (this.$url.value === '') ? 'form' : 'link';
+            this.$element.className = 's3direct ' + status + '-active'
 
-            element.className = 's3direct ' + status + '-active'
+            // add event listeners
+            this.$remove.addEventListener('click', this.removeUpload.bind(this))
+            this.$input.addEventListener('change', this.getUploadURL.bind(this))
 
-
-            // store.subscribe(this.updateScore.bind(this))
-            remove.addEventListener('click', this.removeUpload.bind(this))
-            input.addEventListener('change', this.getUploadURL.bind(this))
-
+            // subscribe to the store so that we can reactively render changes
             store.subscribe(this.render.bind(this));
         }
     }
