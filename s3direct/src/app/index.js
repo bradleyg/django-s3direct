8a1063eecad279a890c44fbd4f7a9d13817ca452
Al Scott <al@yunojuno.com>
2017-06-19 16:50:38 +0100

    remove commented out cruft

--- s3direct/src/app/index.js
+++ s3direct/src/app/index.js
@@ -1,20 +1,6 @@
 import configureStore from './store';
 import {View} from './components';
 
-
-// var addHandlers = function(el) {
-//     var url    = el.querySelector('.file-url'),
-//         input  = el.querySelector('.file-input'),
-//         remove = el.querySelector('.file-remove'),
-//         status = (url.value === '') ? 'form' : 'link'
-
-//     el.className = 's3direct ' + status + '-active'
-
-//     remove.addEventListener('click', removeUpload, false)
-//     input.addEventListener('change', getUploadURL, false)
-// }
-
-
 document.addEventListener('DOMContentLoaded', function(e) {
     const elements = document.querySelectorAll('.s3direct');
 
@@ -24,13 +10,4 @@ document.addEventListener('DOMContentLoaded', function(e) {
         const view = new View(element, store);
         view.init();
     });
-})
-
-// document.addEventListener('DOMNodeInserted', function(e){
-//     if(e.target.tagName) {
-//         var el = e.target.querySelectorAll('.s3direct');
-//         [].forEach.call(el, function (element, index, array) {
-//     addHandlers(element);
-//     });
-//     }
-// })
+});
