dbe4fab73e9fb2ca65ecf83960b78c2db546d1d5
tadeo <tadeo@tadeocarrier.com>
2017-11-02 19:48:26 -0300

    feat: take CSRF token from csrfmiddlewaretoken hidden field also for getAwsV4Signature

--- s3direct/static/s3direct/js/scripts.js
+++ s3direct/static/s3direct/js/scripts.js
@@ -100,10 +100,12 @@ const SparkMD5 = require('spark-md5');
         const getAwsV4Signature = function (signParams, signHeaders, stringToSign, signatureDateTime, canonicalRequest) {
             return new Promise(function (resolve, reject) {
                 const form          = new FormData(),
-                      csrfTokenName = element.querySelector('.csrf-cookie-name').value;
+                      csrfTokenName = element.querySelector('.csrf-cookie-name').value,
+                      csrfInput     = document.querySelector('input[name=csrfmiddlewaretoken]'),
+                      csrfToken     = csrfInput ? csrfInput.value : Cookies.get(csrfCookieNameInput.value),
+                      headers       = {'X-CSRFToken': csrfToken};
                 form.append('to_sign', stringToSign);
                 form.append('datetime', signatureDateTime);
-                const headers = {'X-CSRFToken': Cookies.get(csrfTokenName)};
                 request('POST', signingUrl, form, headers, element, function (status, response) {
                     switch (status) {
                         case 200:
