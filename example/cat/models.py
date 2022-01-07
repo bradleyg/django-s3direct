fc7bff25aa3deb18a50ec39845799cef4858b63d
Safa AlFulaij <salfulaij@lumofy.com>
2022-01-07 21:41:28 +0300

    Minor

--- example/cat/models.py
+++ example/cat/models.py
@@ -3,12 +3,12 @@ from s3direct.fields import S3DirectField
 
 
 class Cat(models.Model):
-    custom_filename = S3DirectField(dest='AVATAR')
+    custom_filename = S3DirectField(dest='custom_filename')
 
 
 class Kitten(models.Model):
     mother = models.ForeignKey('Cat', on_delete=models.CASCADE)
 
-    video = S3DirectField(dest='AVATAR')
-    image = S3DirectField(dest='AVATAR')
-    pdf = S3DirectField(dest='AVATAR')
+    video = S3DirectField(dest='videos')
+    image = S3DirectField(dest='images')
+    pdf = S3DirectField(dest='pdfs')
