bde5a6c1348ca1db0ed77b00cb6bbc4dd99bef45
Bradley Griffiths <bradleyg@users.noreply.github.com>
2022-06-17 08:36:38 +0000

    Fix test warnings and bump packages.

--- runtests.py
+++ runtests.py
@@ -30,6 +30,7 @@ settings.configure(
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
+                    'django.template.context_processors.request',
                     "django.contrib.auth.context_processors.auth",
                     'django.contrib.messages.context_processors.messages',
                 ]
